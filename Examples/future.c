76c4be38992012e2d4ff75f6d27f67db03f79e6c
IrishBruse <Econn50@outlook.com>
2023-05-06 22:03:19 +0100

    Move examples to tests

--- Examples/future.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-void PrintString(char *val) { printf("%s", val); }
-void PrintNumber(int val) { printf("%d", val); }
-
--- Examples/helloworld.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void PrintString(char *val) { printf("%s", val); }
-void PrintNumber(int val) { printf("%d", val); }
-
-int main(){
-
-PrintString("Hello, World!\n");
-}
